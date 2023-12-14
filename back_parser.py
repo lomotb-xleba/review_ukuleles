284d463a9fba1fbc6acd6289385bcb89a48df008
TEMA <tomochkada@gmail.com>
2023-12-15 02:26:09 +0800

    update more cooler

--- back_parser.py
+++ back_parser.py
@@ -6,43 +6,53 @@ aa='3000 5000'
 url="https://guitar-saloon.ru/shop/ukulele/"
 def parser(url:str,aa):
     sys.setrecursionlimit(10000)
-    alf=[" ",",","_","-","=",";",":"]
+    numb=["0","1","2","3","4","5","6","7","8","9"]
     aaa=list(aa)
     n=0
     m=0
     i=0
-    while aaa[i] not in alf:
-        n=n*10+int(aaa[i])
-        i+=1
-    i+=1 
-    while i<len(aaa):
-        m=m*10+int(aaa[i])
-        i+=1
-    base_url = "https://guitar-saloon.ru"
-    r = requests.get(url)
-    soup = bs(r.content, "lxml")
-    products = soup.find_all("div", class_="goods")
-
-    urls = []
-    for product in products:
-        link = product.find('a', itemprop='url').get("href")
-        urls.append(base_url + link)
-    
-    args = []
-    for a in urls:
-        r = requests.get(a)
+    if aaa[i] in numb:
+        while aaa[i] in numb:
+            n=n*10+int(aaa[i])
+            i+=1
+        while aaa[i] not in numb:
+            i+=1
+            if i==len(aaa):
+                args="ERROR"
+                return args
+        while i<len(aaa):
+            if aaa[i] in numb:
+                m=m*10+int(aaa[i])
+                i+=1
+            else:
+                args="ERROR"
+                return args
+        base_url = "https://guitar-saloon.ru"
+        r = requests.get(url)
         soup = bs(r.content, "lxml")
-        name = soup.find("h1", itemprop="name")
-        if name:
-            name=name.get_text(strip=True)
-        price = soup.select_one("span.strong")
-        if price:
-            price=price.get('content')
-            if n<=float(price)<=m:
-        # description = soup.find('div', id='goods_desc')
-        # if description:
-        #     description=description.findNext('div', itemprop='description').text
-                args.append((name, price, a))
+        products = soup.find_all("div", class_="goods")
+
+        urls = []
+        for product in products:
+            link = product.find('a', itemprop='url').get("href")
+            urls.append(base_url + link)
+        
+        args = []
+        for a in urls:
+            r = requests.get(a)
+            soup = bs(r.content, "lxml")
+            name = soup.find("h1", itemprop="name")
+            if name:
+                name=name.get_text(strip=True)
+            price = soup.select_one("span.strong")
+            if price:
+                price=price.get('content')
+                if n<=float(price)<=m:
+                    args.append((name, price, a))
+        if len(args)==0:
+            args=0
+    else:
+        args="ERROR"
     return args       
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -10,31 +10,47 @@ def start(message):
     bot.send_message(message.chat.id, 'Hello, please write price range \n for example: "3000-5000".')
     bot.register_next_step_handler(message, parser)
 def parser(message):
-    bot.send_message(message.chat.id, 'pars..please wait')
+    global arg, j, h
     url="https://guitar-saloon.ru/shop/ukulele/"
     dao=DataAccessObject()
     aa=str(message.text)
+    bot.send_message(message.chat.id, 'pars..please wait')
     a = back_parser.parser(url,aa)
-    dao.init()
-    dao.create(a)
-    arg=dao.show_base()
-    j=0
-    while j<len(f'{arg}'):
-        bot.send_message(message.chat.id, f'{arg[j]}')
-        j+=1
-    #self.bot.send_message(message.chat.id, 'write your price range:')
-#    self.bot.register_next_step_handler(message, get_range)
-# def get_range(message):
-    # dao = database.DataAccessObject()
-    # dao.create(arg)
-    # print(dao)
-    # n, m =map(int(message).split(' '))
-    # for r in dao:
-    #     if r[1]:    
-    #         if n[0]<=float(r[1])<=n[1]:
-    #             print("name   :",r[0])
-    #             print("price  :",r[1])
-    #             print("description   :",r[2])
+    if a==0:
+        bot.send_message(message.chat.id, 'there are no goods in this range \n please try again')
+        bot.register_next_step_handler(message, parser)
+    elif a=="ERROR":
+        bot.send_message(message.chat.id, 'it is not a range \n please write a range \n for example: "3000-5000"')
+        bot.register_next_step_handler(message, parser)
+    else:
+        dao.init()
+        dao.create(a)
+        arg=dao.show_base()
+        j=0; h=1
+        while j+2<30:
+            bot.send_message(message.chat.id, f'{arg[j], arg[j+1],arg[j+2]}')
+            j+=3
+        j=0
+        bot.send_message(message.chat.id, "that's a part of what I found \n do you want more (yes/no)")
+        bot.register_next_step_handler(message, repeater) 
+def repeater(message):
+    global j, h
+    j+=30; h+=1
+    if str(message.text)!="yes" and str(message.text)!="no":
+        bot.send_message(message.chat.id, "please write only yes or no")
+        bot.register_next_step_handler(message, repeater)
+
+    if str(message.text)=="yes" and h*30<len(f'{arg}'):
+        while j+2<h*30:
+            bot.send_message(message.chat.id, f'{arg[j], arg[j+1],arg[j+2]}')
+            j+=3
+
+        bot.send_message(message.chat.id, "that's a part of what I found \n do you want more (yes/no)")
+        bot.register_next_step_handler(message, repeater)
+    if str(message.text)=="no":
+        bot.send_message(message.chat.id, "that's all I found \n you can try another range if you want")
+        bot.register_next_step_handler(message, parser)
+
 
 bot.polling(none_stop=True)
 
